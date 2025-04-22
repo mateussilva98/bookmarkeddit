c2f3eb505716b91fe66e079d5e37dbf2b078c8ca
Mateus Silva <36518616+mateussilva98@users.noreply.github.com>
2025-04-23 00:40:54 +0100

    feat: update header component - remove theme option

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 import styles from "./Header.module.scss";
-import { Sun } from "./icons/Sun";
-import { Moon } from "./icons/Moon";
+/* import { Sun } from "./icons/Sun";
+import { Moon } from "./icons/Moon"; */
 import { Settings } from "./icons/Settings";
 import LOGO from "../assets/images/logo.svg";
 import LOGO_WHITE from "../assets/images/logo_white.svg";
@@ -17,14 +17,17 @@ export const Header: FC<HeaderProps> = ({ onSettingsClick }) => {
   return (
     <header className={styles.root}>
       <img src={store.theme == "dark" ? LOGO_WHITE : LOGO} />
-      <div className={styles.buttons}>
+      {/* <div className={styles.buttons}>
         <button id="settings" className="btn-icon" onClick={onSettingsClick}>
           <Settings />
         </button>
         <button id="theme" className="btn-icon" onClick={changeTheme}>
           {store.theme == "dark" ? <Sun /> : <Moon />}
         </button>
-      </div>
+      </div> */}
+      <button id="settings" className="btn-icon" onClick={onSettingsClick}>
+        <Settings />
+      </button>
     </header>
   );
 };
